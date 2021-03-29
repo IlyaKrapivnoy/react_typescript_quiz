9226c1a5dd03804725f2e8d043d8c574e7f254cb
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-29 12:14:06 +0300

    html

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
 
-function App() {
+const App = () => {
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion = () => {
+
+  }
+
   return (
     <div>
-      Quiz
+      <h1>REACT QUIZ</h1>
+      <button 
+        className="start"
+        onClick={startTrivia}
+      >
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p>Loading Question...</p>
     </div>
   );
 }
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const QuestionCard = () => <div>Question CArd</div>
+
+export default QuestionCard;
