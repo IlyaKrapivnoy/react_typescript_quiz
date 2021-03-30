43e4d393664009b86fa77bc6f1c476b7244202ad
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-03-30 19:38:26 +0300

    Next Question

--- src/App.tsx
+++ src/App.tsx
@@ -62,20 +62,24 @@ const App = () => {
       : null}
       {!gameOver ? <p className="score">Score:</p> : null}
       {loading && <p>Loading Question...</p>}
-      {/* <QuestionCard 
+      {!loading && !gameOver &&
+        (<QuestionCard 
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      /> */}
-      <button 
-        className="next"
-        onClick={nextQuestion}
-      >
-        Next Question
-      </button>
+        />)
+      }
+      {!gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 ? (
+        <button 
+          className="next"
+          onClick={nextQuestion}
+        >
+          Next Question
+        </button>
+      ) : null}
     </div>
   );
 }
